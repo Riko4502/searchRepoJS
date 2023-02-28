8c5e7ffd702dec2df543a04c6e38976bc8202ae7
Riko4502 <riko4502@gmail.com>
2023-02-28 23:00:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,110 @@
+const input = document.querySelector('.search-input');
+const search = document.querySelector('.search');
+const repo = document.querySelector('.repo');
+
+input.addEventListener("keyup", debounce(showRequest, 450));
+
+function debounce(fn, ms) {
+    let timer;
+
+    return function () {
+        const fnCall = () => {
+            fn.apply(this, arguments);
+        };
+
+        clearTimeout(timer);
+        timer = setTimeout(fnCall, ms);
+    };
+}
+
+function searchData(data) {
+    let link = document.createElement("div");
+
+    link.insertAdjacentHTML("afterbegin",
+        `<div class ='search-link'>${data}</div>`
+    );
+
+    return link;
+}
+
+function addRepoLIst(data) {
+    let add = document.createElement("div");
+    add.className = "repo-list";
+
+    add.insertAdjacentHTML("afterbegin",
+      `
+        <div class='text'>Name: ${data.name}</div>
+        <div class='text'>Owner: ${data.owner["login"]}</div>
+        <div class='text'>Stars: ${data.stargazers_count}</div>
+      `
+    );
+  
+    add.append(delRepoList(add));
+  
+    return add;
+}
+  
+function delRepoList(data) {
+    const repoClose = document.createElement("div");
+    repoClose.classList.add("repo-close");
+
+    repoClose.addEventListener("click", () => {
+        data.remove();
+    });
+    
+    repoClose.removeEventListener("click", () => {
+        data.remove();
+    });
+  
+    return repoClose;
+}
+
+async function showRequest(e) {
+    input.value = input.value.trim();
+
+    if (input.value.length === 0 && input.value === "") {
+        return search.style.display = "none";
+    }
+
+    search.style.display = "block";
+    search.textContent = "";
+
+    try {
+        const response = await fetch(
+            `https://api.github.com/search/repositories?q=${input.value}&per_page=5`
+        );
+
+        let data = await response.json();
+        data.items.map((item) => {
+            search.append(searchData(item.name));
+        });
+
+        search.addEventListener("click", (e) => {
+            for (let i = 0; i < data.items.length; i++) {
+                if (e.target.innerText == data.items[i].name) {
+                    repo.prepend(addRepoLIst(data.items[i]));
+
+                    e.target.innerText = "";
+                    search.textContent = "";
+                }
+            }
+
+            input.value = "";
+        });
+
+        search.removeEventListener("click", (e) => {
+            for (let i = 0; i < data.items.length; i++) {
+                if (e.target.innerText == data.items[i].name) {
+                    repo.prepend(addRepoLIst(data.items[i]));
+
+                    e.target.innerText = "";
+                    search.textContent = "";
+                }
+            }
+
+            input.value = "";
+        });    
+    } catch (err) {
+        console.error('Ошибка:', err);
+    }
+}
